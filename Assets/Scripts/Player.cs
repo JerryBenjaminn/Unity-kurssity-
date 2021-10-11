5a5daac60c4658b462d4326a88e26c2fcbb8a7c6
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-10-11 13:05:51 +0300

    Added a sprite for the player and attached script to the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float playerSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
