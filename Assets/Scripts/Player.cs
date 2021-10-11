43573ffcc1b4d11356c3a7962cb05567439bc020
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-10-11 14:00:03 +0300

    Added camera movement and background

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    //M‰‰ritell‰‰n muuttujat pelaajan liikuttamiseen
     public float playerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); //Pelaajan rigidbody-komponenttia kutsutaan, kun peli aloitetaan
     }
 
     
     void Update()
     {
-        
+        float directionY = Input.GetAxisRaw("Vertical"); //M‰‰ritell‰‰n pelaajan liike y-akselilla, kun painetaan haluttua n‰pp‰int‰ 
+        playerDirection = new Vector2(0, directionY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, playerDirection.y * playerSpeed); //Kun pelaajalle on m‰‰ritelty liike, niin sen nopeutta voidaan s‰‰t‰‰ Unityss‰ ilman, ett‰ scripti‰ tarvitsee muuttaa
     }
 }
